7bb428c380233017042f4248f46652dbcfd0be94
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-03 16:58:37 -0700

    update src files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,9 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-simple-mocha');
 
-	var srcFiles = ['Gruntfile.js'];
+	var srcFiles = ['Gruntfile.js', 'index.js'];
 
 	grunt.initConfig({
 
